7d1209954dc0e7d9c9110b7149bc34a303eba76d
Anniek-W <150136@csgaugustinus.nl>
2020-03-25 11:19:57 +0000

    demo

--- public/PHP/opdracht_2.php
+++ public/PHP/opdracht_2.php
@@ -7,8 +7,15 @@ TYP HIERONDER JOUW PHPCODE
 
 $tekst="Ik heb een bijbaantje bij de Aldi. De Aldi betaalt goed.";
 echo "$tekst";
-//$tekst=str_replace('Aldi','Albert Heijn',$tekst);
-echo '<h4>$tekst</h4>';
+$tekst=str_replace('Aldi','Albert Heijn',$tekst);
+$tekst=strstr($tekst,"De");
+echo '<h4>'.$tekst.'</h4>';
+$lengte=strlen ($tekst);
+echo '<i>de string $tekst bestaat uit <b>'.$lengte.'</b> karakters</i><br>';
+$tekst=substr($tekst,10,5);
+echo "$tekst<br>";
+echo strrev($tekst).'<br>';
+echo str_repeat($lengte,13);
 
 /****************************
 EINDE VAN JOUW PHPCODE
