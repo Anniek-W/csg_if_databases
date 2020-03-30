2e04c85497d572276c175aba35bae58e9d49f363
Anniek-W <150136@csgaugustinus.nl>
2020-03-30 12:38:53 +0000

    demo

--- public/PHP/opdracht_4b.php
+++ public/PHP/opdracht_4b.php
@@ -5,12 +5,17 @@ include('opdracht_begin.php');
 TYP HIERONDER JOUW PHPCODE
 ****************************/
 
-$leerling=array('nr' => 123456, 'voornaam' => 'Alan', 'achternaam' => 'Turing');
+$leerling=array('nr' => 150136, 'voornaam' => 'Anniek', 'achternaam' => 'van Wigcheren', 'geboortejaar' => '2003');
 
 echo "<pre>Leerling:";
 print_r($leerling);
 echo "</pre>";
 
+echo $leerling['voornaam']." ".$leerling['achternaam']." werd geboren in ".$leerling['geboortejaar'];
+echo '<br>';
+echo current($leerling);
+echo '<br>';
+echo end($leerling);
 /****************************
 EINDE VAN JOUW PHPCODE
 ****************************/
